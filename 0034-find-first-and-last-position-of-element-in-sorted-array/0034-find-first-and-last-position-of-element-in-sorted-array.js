2f2015014cddd9255d4917a6518d9a621446329c
vasureddy413 <vasureddykotha9515@gmail.com>
2026-06-26 20:49:35 +0530

    Time: 0 ms (100%), Space: 53.1 MB (98.09%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+
+    function findFirst() {
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) {
+                ans = mid;
+                right = mid - 1; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    function findLast() {
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) {
+                ans = mid;
+                left = mid + 1; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    return [findFirst(), findLast()];
+};
+
+    
