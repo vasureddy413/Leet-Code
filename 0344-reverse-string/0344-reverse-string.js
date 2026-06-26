90a5c52eb76275aa03b48acb2c06fcf01c376bb7
vasureddy413 <vasureddykotha9515@gmail.com>
2026-06-26 20:39:21 +0530

    Time: 0 ms (100%), Space: 60.6 MB (78.23%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {character[]} s
+ * @return {void}
+ */
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+};
+
+let s = ["h", "e", "l", "l", "o"];
+reverseString(s);
+console.log(s);
